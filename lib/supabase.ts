8bdab695463ec03fb042598fbf0717eb6006feff
Kinford Tech <kinfordschool@gmail.com>
2026-05-18 14:36:24 +0530

    refactor: Support NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in Render

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ecazlhecquqtrkcrvejw.supabase.co'
-const supabaseAnonKey = 'sb_publishable_zZayN5u637gqPn2h-QqlZw_yYhQ_ZjC'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ecazlhecquqtrkcrvejw.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_zZayN5u637gqPn2h-QqlZw_yYhQ_ZjC'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
